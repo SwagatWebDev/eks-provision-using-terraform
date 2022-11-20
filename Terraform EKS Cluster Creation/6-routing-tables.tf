40df06973a8548caa822aa766392f07dd58123b8
swagat030 <swagatkumarmishra888@gmail.com>
2022-11-20 23:10:19 +0530

    Script updated

---
+++ Terraform EKS Cluster Creation/6-routing-tables.tf
@@ -0,0 +1,17 @@
+# A Route table contains a set of rules, called routes, that are used to determine where 
+# network traffic from your subnet or gateway is directed.
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    # The CIDR block of the route.
+    cidr_block = "10.0.1.0/24"
+    # Directed the traffic to the Internet gateway 
+    gateway_id = aws_internet_gateway.example.id
+  }
+
+  tags = {
+    Name = "public"
+  }
+}
