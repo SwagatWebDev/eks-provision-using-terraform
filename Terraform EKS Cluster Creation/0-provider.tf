7643cedfdaa751606b9cff8e711a50afd98a69ec
swagat030 <swagatkumarmishra888@gmail.com>
2022-11-22 12:01:22 +0530

    Update 0-provider.tf

--- Terraform EKS Cluster Creation/0-provider.tf
+++ Terraform EKS Cluster Creation/0-provider.tf
@@ -2,7 +2,6 @@
 # supported by AWS, such as VPC, EC2, EKS, and many others. You must configure the provider 
 # with the proper credentials before using it. 
 provider "aws" {
-    profile = "terraform"
     region = "us-east-2"
 }
 
