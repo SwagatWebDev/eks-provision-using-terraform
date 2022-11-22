4c7888ab04eed88629d31dd703f7d9e1c571da11
swagat030 <swagatkumarmishra888@gmail.com>
2022-11-22 10:52:12 +0530

    File updated

--- Terraform EKS Cluster Creation/0-provider.tf
+++ Terraform EKS Cluster Creation/0-provider.tf
@@ -1,10 +1,6 @@
 # We need to create an AWS provider. It allows to interact with the many resources 
 # supported by AWS, such as VPC, EC2, EKS, and many others. You must configure the provider 
-# with the proper credentials before using it. The most common authentications methods:
-# 1. AWS shared credentials/configuration files
-# 2. Environment variables
-# 3. Static credentials
-# 4. EC2 instance metadata
+# with the proper credentials before using it. 
 provider "aws" {
     profile = "terraform"
     region = "us-east-2"
